842f4bc316d4b939b79673e1899ae736a219e4dd
SimonAmar26 <150928602+SimonAmar26@users.noreply.github.com>
2024-05-01 09:56:00 +0200

    Update run.py

--- run.py
+++ run.py
@@ -17,6 +17,8 @@ import wandb
 from PIL import Image
 import torchvision.transforms.v2 as transforms
 
+print('hellloooooo')
+
 # UTILITIES
 
 def extract_frames(video_path, nb_frames=10, delta=1, timeit=False):
