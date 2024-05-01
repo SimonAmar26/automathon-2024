4ec26dad9982c3b94ae29a089346325f7f595dc8
SimonAmar26 <150928602+SimonAmar26@users.noreply.github.com>
2024-05-01 10:25:13 +0200

    Update run.py

--- run.py
+++ run.py
@@ -217,6 +217,7 @@ class VideoDataset(Dataset):
 
 
 train_dataset = VideoDataset(dataset_dir, dataset_choice="train", nb_frames=nb_frames)
+print(train_dataset)
 test_dataset = VideoDataset(dataset_dir, dataset_choice="test", nb_frames=nb_frames)
 experimental_dataset = VideoDataset(dataset_dir, dataset_choice="experimental", nb_frames=nb_frames)
 
@@ -224,17 +225,42 @@ experimental_dataset = VideoDataset(dataset_dir, dataset_choice="experimental", 
 # MODELE
 
 class DeepfakeDetector(nn.Module):
-    def __init__(self, nb_frames=10):
-        super().__init__()
-        self.dense = nn.Linear(nb_frames*3*256*256,1)
-        self.flat = nn.Flatten()
+    def __init__(self, nb_frames):
+        super(DeepfakeDetector, self).__init__()
+        self.nb_frames = nb_frames
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5, padding=2)
+        
+        self.bn1 = nn.BatchNorm2d(8)
+        self.bn2 = nn.BatchNorm2d(8)
+        self.bn3 = nn.BatchNorm2d(16)
+        
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool3 = nn.MaxPool2d(kernel_size=4, stride=4)
+        
+        self.fc1 = nn.Linear(1024, 16)
+        self.fc2 = nn.Linear(16, 1)
+        
+        self.dropout = nn.Dropout(0.5)
+        
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        y = self.flat(x)
-        y = self.dense(y)
-        y = self.sigmoid(y)
-        return y
+        bs, nf, c, h, w = x.size()
+        x = x.view(bs * nf, c, h, w)
+        
+        x = self.pool1(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool2(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool3(F.relu(self.bn3(self.conv3(x))))
+        
+        x = x.view(bs, nf * -1) 
+
+        x = F.relu(self.fc1(self.dropout(x)))
+        x = self.sigmoid(self.fc2(self.dropout(x)))
+        
+        return x
 
 # LOGGING
 
